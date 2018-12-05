95bc87bd99e69bb6b4fa45a20589a81a13a460a1
YanLo <logovoya@tut.by>
2018-12-05 22:51:12 +0300

    compile

--- theormeh/rungekut.c
+++ theormeh/rungekut.c
@@ -5,8 +5,8 @@
 
 enum constants
 {
-  number_of_iterations = 10,
-}
+  num_of_it = 10,
+};
 
 double* vec_sum(double* a, double* b, int dim)
 {
@@ -27,15 +27,15 @@ double* mul_vec_and_num(double* a, int dim, double num)
 
 }
 
-double* rungekut((double*) func(double x, double* y), int dim, 
+double* rungekut(double* (*func)(double , double* , int ), int dim,
               double x_0, double* y_0, double x_1)
 {
-  double k_1[dim], k_2[dim], k_3[dim], k_4[dim];
-  double h = (x_1 - x_0) / number_of_iterations;
+  //double k_1[dim], k_2[dim], k_3[dim], k_4[dim];
+  double h = (x_1 - x_0) / num_of_it;
   int i = 0;
-  double x_cur, *y_cur;
+  double x_cur, *y_cur, *k_1, *k_2, *k_3, *k_4;
 
-  for(i = 1; i <= number_of_iterations; i++)
+  for(i = 1; i <= num_of_it; i++)
   {
     /*
      *  k_1 = f(x_n, y_n)
@@ -43,17 +43,20 @@ double* rungekut((double*) func(double x, double* y), int dim,
      *  k_3 = f(x_n + h/2, y_n + h/2 * k_2);
      *  k_4 = f(x_n + h, y_n + h * k_3);
      */
-    k_1 = func(x_cur, y_cur);
-    k_2 = func(x_cur + h / 2, vec_sum(y_cur, mul_vec_and_num(k_1, dim, h / 2), dim);
-    k_3 = func(x_cur + h / 2, vec_sum(y_cur, mul_vec_and_num(k_2, dim, h / 2), dim);
-    k_4 = func(x_cur + h, vec_sum(y_cur, mul_vec_and_num(k_3, dim, h), dim);
+    k_1 = func(x_cur, y_cur, dim);
+    k_2 = func(x_cur + h / 2, vec_sum(y_cur, mul_vec_and_num(k_1, dim, h / 2), dim), dim);
+    k_3 = func(x_cur + h / 2, vec_sum(y_cur, mul_vec_and_num(k_2, dim, h / 2), dim), dim);
+    k_4 = func(x_cur + h, vec_sum(y_cur, mul_vec_and_num(k_3, dim, h), dim), dim);
 
     //y_{n+1} = y_n + h/6 * (k_1 + 2*k_2 + 2*k_3 + k_4);
     
     //1. h/6 * k_1 + h/3 * k_2
-    double temp_1 = vec_sum(mul_vec_and_num(k_1, dim, h/6), mul_vec_and_num(k_2, dim, h/3), dim);
-
-
-
+    double* temp_1 = vec_sum(mul_vec_and_num(k_1, dim, h/6), mul_vec_and_num(k_2, dim, h/3), dim);
+    //1. h/3 * k_3 + h/6 * k_4
+    double* temp_2 = vec_sum(mul_vec_and_num(k_3, dim, h/3), mul_vec_and_num(k_4, dim, h/6), dim);
+    double* temp_3 = mul_vec_and_num(vec_sum(temp_1, temp_2, dim), dim, h/6);
+    y_cur = vec_sum(y_cur, temp_3, dim);
   }
+
+  return y_cur;
 }
--- theormeh/rungekut.h
+++ theormeh/rungekut.h
@@ -1,12 +1,12 @@
 #ifndef RUNGE
 #define RUNGE
 
+double* vec_sum(double* a, double* b, int dim);
+double* mul_vec_and_num(double* a, int dim, double num);
+double* rungekut(double* (*func)(double , double* , int ), int dim,
+              double x_0, double* y_0, double x_1);
 
-double* vector_sum(double* a, double* b, int dimension);
 
-
-double* rungekut((double*) func, int dimension, 
-              double x_0, double y_0, double x_1);
 
 
 
