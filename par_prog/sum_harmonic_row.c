11fd5c7ae30dd3f013495e9faf3d987d5f501081
YanLo <logovoya@tut.by>
2020-03-18 12:09:09 +0300

    compute sum

--- par_prog/sum_harmonic_row.c
+++ par_prog/sum_harmonic_row.c
@@ -1,14 +1,19 @@
 #include <mpi.h>
 #include <stdio.h>
 
-# define N 16
+enum
+{
+    N = 16
+};
+
+//we are going to compute sum(1/k), k from 1 to N
 
 int main(int argc, char* argv[])
 {
     MPI_Init(&argc, &argv);
-    int P, rank;
+    int P, rank; //P - number of proccesses involved, rank - process id
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &P);
+    MPI_Comm_size(MPI_COMM_WORLD, &P); //MPI_COMM_WORLD - id of global communcator
 
     float num = 0, sum = 0, j = 0;
     int i = 0, el_per_proc = N / P;
@@ -19,9 +24,10 @@ int main(int argc, char* argv[])
             for(j = ((el_per_proc*i) + 1); j < (el_per_proc*(i + 1) + 1); j++)
                 sum += 1/j;
             if (i != 0)
-            MPI_Send(&sum, 1, MPI_FLOAT, 0, 0, MPI_COMM_WORLD);
+                MPI_Send(&sum, 1, MPI_FLOAT, 0, 0, MPI_COMM_WORLD);
         }
 
+    //we need proccess with id = 0, because it has access to the console we working with
     if (rank == 0)
     { //master
         if (N % P != 0)
