c476d9dad47618fef6ad968b0dc8260f5e4d1d7b
YanLo <logovoya@tut.by>
2020-04-19 18:18:53 +0300

    update

--- par_prog/transfer_eq_lab/delay.c
+++ par_prog/transfer_eq_lab/delay.c
@@ -9,20 +9,18 @@ int main(int argc, char* argv[])
     MPI_Comm_size(MPI_COMM_WORLD, &num_of_proc);
 
     int num = 0;
+    double t1, t2;
 
-    if (rank == 0)
-    {
+    t1 = MPI_Wtime();
+    if (rank == 0) {
         num += 1;
-        if (num_of_proc > 1)
-            MPI_Send(&num, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+        MPI_Send(&num, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
     }
-    else
-    {
-        MPI_Recv(&num, 1, MPI_INT, rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    else {
+        MPI_Recv(&num, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         printf("Recieved: %d, rank: %d\n", num, rank);
-        num += 1;
-        if (rank != (num_of_proc - 1))
-            MPI_Send(&num, 1, MPI_INT, rank + 1, 0, MPI_COMM_WORLD);
+        t2 = MPI_Wtime();
+        printf("Delay: %f\n", t2 - t1);
     }
     MPI_Finalize();
 }
