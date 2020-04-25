9b527f2dd15a5b766625b7cb9e2dc0823029a405
YanLo <logovoya@tut.by>
2020-04-25 16:59:41 +0300

    update

--- par_prog/transfer_eq_lab/parallel.c
+++ par_prog/transfer_eq_lab/parallel.c
@@ -5,16 +5,23 @@
 
 int T, X, K, M;
 double **u;
+double *sendbuf, *recvbuf;
 double dt, dx;
 
-double  func(double t, double x);
-double  psi(double t);
-double  phi(double x);
-void    fill_init_conditions();
-void    fill_u_matrix(int rank, int size);
-void    fill_border_conditions(int k);
-void    compute_u_cells(int k, int rank, int size);
-void    dump_data();
+enum
+{
+    root = 0,
+    max_proc_quantity = 16,
+};
+
+double  func(double, double);
+double  psi(double);
+double  phi(double);
+void    initValues();
+void    fill_and_send(int, int);
+void    receive_compute_send_cells(int, int, int, int, int);
+void    fill_border_values_and_send();
+void    gather_and_dump_data(int);
 void    free_memory();
 
 int main(int argc, char **argv) {
@@ -24,45 +31,39 @@ int main(int argc, char **argv) {
     X = (argc >= 4) ? atoi( argv[3] ) : 10;
     M = (argc >= 5) ? atoi( argv[4] ) : 5;
 
-    fill_init_conditions();
+    initValues();
 
     MPI_Init(&argc, &argv);
     int size, rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    fill_u_matrix(rank, size);
+    if (rank != root) {
+        fill_and_send(rank, size);
+    }
 
-    if (rank == 0) {
-        dump_data();
+    if (rank == root) {
+        fill_border_values_and_send();
+        gather_and_dump_data(size);
         free_memory();
     }
     MPI_Finalize();
     return 0;
 }
 
-void fill_u_matrix(int rank, int size) {
+void fill_border_values_and_send() {
 
     int k;
     for (k = 0; k < (K - 1); k++) {
-        if (rank == 0) {
-            fill_border_conditions(k);
-        } else {
-            compute_u_cells(k, rank, size);
-        }
+        u[k + 1][0] = psi((k + 1) * dt);
+        MPI_Send(&(u[k + 1][0]), 1, MPI_FLOAT, root + 1, 0, MPI_COMM_WORLD);
         MPI_Barrier(MPI_COMM_WORLD);
     }
 }
 
-void fill_border_conditions(int k) {
+void fill_and_send(int rank, int size) {
 
-    u[k + 1][0] = psi((k + 1) * dt);
-
-}
-
-void compute_u_cells(int k, int rank, int size) {
-
-    int m, m_start, m_finish;
+    int k, m, m_start, m_finish;
     int num_of_columns_to_fill = (M - 1) / size;
 
     m_start = num_of_columns_to_fill * (rank - 1) + 1;
@@ -73,28 +74,92 @@ void compute_u_cells(int k, int rank, int size) {
         m_finish = num_of_columns_to_fill * rank;
     }
 
-    printf("hi from rank %d\n", rank);
-    fflush(stdout);
+
+    for (k = 0; k < (K - 1); k++) {
+        receive_compute_send_cells(k, rank, size, m_start, m_finish);
+        MPI_Barrier(MPI_COMM_WORLD);
+    }
+
+
+    for (k = 1; k < K; k++) {
+        for (m = m_start; m <= m_finish; m++) {
+            sendbuf[(k - 1) * (M - 1) + m - 1] = u[k][m];
+        }
+    }
+
+    MPI_Gather(sendbuf, (K - 1) * (M - 1), MPI_DOUBLE, recvbuf, (K - 1) * (M - 1), MPI_DOUBLE, root, MPI_COMM_WORLD);
+}
+
+
+
+void receive_compute_send_cells(int k, int rank, int size, int m_start, int m_finish) {
+
+    printf("Hi from %d from %d, I am filling my data\n", rank, size);
+    if (k > 0) {
+        MPI_Recv(&(u[k][m_start - 1]), 1, MPI_FLOAT, rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        printf("%d have receieved\n", rank);
+    }
+
+    int m;
     for (m = m_start; m <= m_finish; m++) {
         u[k + 1][m] = u[k][m] + dt * (func(k * dt, m * dx) -
                                         (u[k][m] - u[k][m - 1]) / dx);
     }
 
+    u[k][m_start - 1] = 0; //we don't need it anymore
+
+    if (rank < (size - 1)) {
+        MPI_Send(&(u[k + 1][m_finish]), 1, MPI_FLOAT, rank + 1, 0, MPI_COMM_WORLD);
+    }
+    printf("%d have send\n", rank);
+
 }
 
-void fill_init_conditions() {
+void gather_and_dump_data(int size) {
+
+    MPI_Gather(sendbuf, (K - 1) * (M - 1), MPI_DOUBLE, recvbuf, (K - 1) * (M - 1), MPI_DOUBLE, root, MPI_COMM_WORLD);
+
+    int k, m, rank;
+    for (rank = 1; rank < size; rank++) {
+        for (k = 1; k < K; k++) {
+            for (m = 1; m < M; m++) {
+                u[k][m] = recvbuf[(K - 1) * (M - 1) * rank + (k - 1) * (M - 1) + m - 1];
+            }
+        }
+    }
+
+    FILE* data_file = fopen("parallel_data.txt", "wb");
+
+    for (k = 0; k < K; k++) {
+        fprintf(data_file, "%d time_step:\t\t", k);
+        for (m = 0; m < M; m++) {
+            fprintf(data_file, "%.3f\t\t", u[k][m] );
+        }
+        fprintf(data_file, "\n");
+    }
+
+    fclose(data_file);
+}
+
+void initValues() {
 
     dx = (double) X / M;
     dt = (double) T / K;
 
-    u = (double**) malloc(sizeof(double*) * K);
+    u = (double**) calloc(K, sizeof(double*));
     assert(u);
 
-    int i;
-    for (i = 0; i < K; i++) {
-        u[i] = (double*) malloc(sizeof(double) * M);
-        assert(u[i]);
+    int k;
+    for (k = 0; k < K; k++) {
+        u[k] = (double*) calloc(M, sizeof(double));
+        assert(u[k]);
     }
+
+    sendbuf = (double*) calloc((M - 1) * (K - 1), sizeof(double));
+    assert(sendbuf);
+    recvbuf = (double*) calloc((M - 1) * (K - 1) * max_proc_quantity, sizeof(double));
+    assert(recvbuf);
+
 
     int m;
     for (m = 0; m < M; m++) {
@@ -114,21 +179,6 @@ double psi(double t) {
     return t * t;
 }
 
-void dump_data() {
-    FILE* data_file = fopen("parallel_data.txt", "wb");
-
-    int k, m;
-    for (k = 0; k < K; k++) {
-        fprintf(data_file, "%d time_step:\t\t", k);
-        for (m = 0; m < M; m++) {
-            fprintf(data_file, "%.3f\t\t", u[k][m] );
-        }
-        fprintf(data_file, "\n");
-    }
-
-    fclose(data_file);
-}
-
 void free_memory() {
     int i;
     for (i = 0; i < K; i++) {
@@ -136,4 +186,6 @@ void free_memory() {
     }
 
     free(u);
+    free(sendbuf);
+    free(recvbuf);
 }
